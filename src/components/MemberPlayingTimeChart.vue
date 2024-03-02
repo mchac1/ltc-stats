cf715766f17283cea180c1113fb1af91455a7a9a
mchardyc <mchardychad@hotmail.com>
2024-03-02 09:06:15 -0600

    try spinner

--- src/components/MemberPlayingTimeChart.vue
+++ src/components/MemberPlayingTimeChart.vue
@@ -1,13 +1,15 @@
 <template>
     <div style="border:1px solid black; padding: 25px; margin-bottom: 50px; text-align:center;">
         <h2>{{ mapTitle }}</h2>
-        <button @click="renderChart" value="">All-time</button>
-        <button @click="renderChart" value="2023">2023</button>
-        <button @click="renderChart" value="2022">2022</button>
-        <button @click="renderChart" value="2021">2021</button>
-        <button @click="renderChart" value="2020">2020</button>
-        <!-- <canvas id="MemberPlayingTimeChart" width="800" height="800"></canvas> -->
-        <canvas id="MemberPlayingTimeChart" width="1080" height="650"></canvas>
+        <div v-if="isLoading" class="spinner-border m-5" role="status"></div>
+        <div v-else>
+            <button @click="renderChart" value="">All-time</button>
+            <button @click="renderChart" value="2023">2023</button>
+            <button @click="renderChart" value="2022">2022</button>
+            <button @click="renderChart" value="2021">2021</button>
+            <button @click="renderChart" value="2020">2020</button>
+        </div>
+        <canvas id="MemberPlayingTimeChart" width="1080" height="650" :style="chartVisibility"></canvas>
     </div>
 </template>
   
@@ -23,65 +25,26 @@
         memberBookingsData: [],
         currentChart: null,
         chartConfig: {},
+        isLoading: null,
       }
+    },
+    computed: {
+        chartVisibility() {
+            if (this.isLoading) {
+                return "visibility: hidden;"
+            }
+            return "visibility: visible;"
+        },
     },
     methods: {
         async renderChart(event) {
-            this.currentChart.destroy();
+            this.isLoading = true;
             await this.configureChart(event.target._value);
+            this.isLoading = false;
         },
-        // async renderChart(event) {
-        //     this.currentChart.destroy();
-
-        //     await this.fetchMemberBookingsData(event.target._value);
-
-        //     this.mapTitle = 'Top Court Reservers (All-time)';
-        //     if (event.target._value) {
-        //         this.mapTitle = `Top Court Reservers (${event.target._value})`;
-        //     }
-            
-        //     this.chartConfig = {
-        //         type: 'bar',
-        //         data: {
-        //             labels: this.memberBookingsData.map(a => a._id.member),
-        //             datasets: [
-        //                 {
-        //                     label: 'Bookings Created',
-        //                     data: this.memberBookingsData.map(a => a.totalQuantity)
-        //                 }
-        //             ]
-        //         },
-        //         options: {
-        //             indexAxis: 'y',
-        //             elements: {
-        //                 bar: {
-        //                     borderWidth: 2,
-        //                 }
-        //             },
-        //             responsive: true,
-        //             plugins: {
-        //                 title: {
-        //                     display: true,
-        //                     // text: mapTitle
-        //                 }
-        //             }
-        //         },
-        //     }
-
-        //     let myChart = new Chart(
-        //         document.getElementById('MemberPlayingTimeChart'),
-        //         this.chartConfig
-        //     );
-
-        //     this.currentChart = myChart;
-        // },
         async fetchMemberBookingsData(year) {
-            // let urlAddress = `http://localhost:3000/api/players/getReservationCountByMember`;
-            // let urlAddress = `${import.meta.env.VITE_MONGODB_URI}/getReservationCountByMember`;
             let urlAddress = `${import.meta.env.VITE_MONGODB_URI}/getMemberHours`;
             if (year) {
-                // urlAddress = `http://localhost:3000/api/players/getReservationCountByMember?Year=${year}`;
-                // urlAddress = `${import.meta.env.VITE_MONGODB_URI}/getReservationCountByMember?Year=${year}`;
                 urlAddress = `${import.meta.env.VITE_MONGODB_URI}/getMemberHours?Year=${year}`;
             }
             const url = new URL(urlAddress);
@@ -93,16 +56,12 @@
                 data.sort((a, b) => b.totalQuantity - a.totalQuantity);
                 // Just get top 20
                 this.memberBookingsData = data.slice(0, 15);
-                console.log('CAM members data')
-                console.log(this.memberBookingsData)
             });
         },
         async configureChart(year) {
+
             await this.fetchMemberBookingsData(year);
 
-            // console.log(this.familyBookingsData)
-
-            // this.mapTitle = 'Top Court Reservers (All-time)';
             this.mapTitle = 'Top Individual Hours on Court (All-time)';
             if (year) {
                 this.mapTitle = `Top Individual Hours on Court (${year})`;
@@ -111,7 +70,6 @@
             this.chartConfig = {
                 type: 'bar',
                 data: {
-                    // labels: this.memberBookingsData.map(a => a.familyName),
                     labels: this.memberBookingsData.map(a => a.name),
                     datasets: [
                         {
@@ -137,10 +95,8 @@
                     plugins: {
                         title: {
                             display: true,
-                            // text: mapTitle
                         }
                     },
-                    // barThickness: 25,
                     scales: {
                         x: {
                             stacked: true,
@@ -152,73 +108,20 @@
                 },
             }
 
+            if (this.currentChart) {
+                this.currentChart.destroy();
+            }
+
             this.currentChart = new Chart(
-                // document.getElementById('FamilyPlayingTimeChart'),
                 document.getElementById('MemberPlayingTimeChart'),
                 this.chartConfig
             );
         }
     },
     async mounted() {
+        this.isLoading = true;
         await this.configureChart('2023');
+        this.isLoading = false;
     }
-    // async mounted() {
-    //     // Initially get all-time bookings
-    //     await this.fetchMemberBookingsData();
-
-    //     this.chartConfig = {
-    //         type: 'bar',
-    //         data: {
-    //             labels: this.memberBookingsData.map(a => a._id.member),
-    //             datasets: [
-    //                 {
-    //                     label: 'Bookings Created',
-    //                     data: this.memberBookingsData.map(a => a.totalQuantity)
-    //                 }
-    //             ]
-    //         },
-    //         options: {
-    //             indexAxis: 'y',
-    //             elements: {
-    //                 bar: {
-    //                     borderWidth: 2,
-    //                 }
-    //             },
-    //             responsive: true,
-    //             plugins: {
-    //                 title: {
-    //                     display: true,
-    //                     text: 'Top Court Reservers (All-time)'
-    //                 }
-    //             }
-    //         },
-    //     }
-  
-    //   let myChart = new Chart(
-    //     document.getElementById('MemberPlayingTimeChart'),
-    //     this.chartConfig
-    //   );
-
-    //   this.currentChart = myChart;
-    // }
   }
   </script>
-  
-  <!-- Add "scoped" attribute to limit CSS to this component only -->
-  <style scoped>
-  h3 {
-    margin: 40px 0 0;
-  }
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
-  li {
-    display: inline-block;
-    margin: 0 10px;
-  }
-  a {
-    color: #42b983;
-  }
-  </style>
-  
--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { createPinia } from 'pinia'
 import piniaPluginPersistedState from "pinia-plugin-persistedstate"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
 
 const pinia = createPinia();
 pinia.use(piniaPluginPersistedState)
